204416d60910995c75ea735f4270f5bfad6b86bd
kaiovinic <kaiovini19@hotmail.com>
2023-10-02 22:47:45 -0300

    Página taskList

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,12 +1,37 @@
 import React from "react";
 
-type Props = {};
+// Interface
+import { ITask } from "../interfaces/Task";
 
-const TaskList = (props: Props) => {
+// CSS
+import styles from "./TaskList.module.css";
+
+type Props = {
+  taskList: ITask[];
+};
+
+const TaskList = ({ taskList }: Props) => {
   return (
-    <div>
-      <p>Lista de tarefas</p>
-    </div>
+    <>
+      {taskList.length > 0 ? (
+        taskList.map((task) => {
+          return (
+            <div key={task.id}>
+              <div>
+                <h4>{task.title}</h4>
+                <p>{task.difficulty}</p>
+              </div>
+              <div>
+                <i className="bi bi-pencil"></i>
+                <i className="bi bi-trash"></i>
+              </div>
+            </div>
+          );
+        })
+      ) : (
+        <p>Não tem tarefas cadastradas</p>
+      )}
+    </>
   );
 };
 
