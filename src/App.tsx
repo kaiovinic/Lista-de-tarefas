dd174279606017b41efd9d9fef907e2175bb2888
kaiovinic <kaiovini19@hotmail.com>
2023-10-03 23:32:16 -0300

    Criação do Modal

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,44 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import TaskForm from "./components/TaskForm";
 import TaskList from "./components/TaskList";
+import Modal from "./components/Modal";
 
 // Interface
 import { ITask } from "./interfaces/Task";
 
 function App() {
   const [taskList, setTaskList] = React.useState<ITask[]>([]);
+  const [taskToUpdate, setTaskToUpdate] = React.useState<ITask | null>(null);
 
   const deleteTask = (id: number) => {
     setTaskList(taskList.filter((task) => task.id !== id));
   };
 
+  const hideOrShowModal = (display: boolean) => {
+    const modal = document.getElementById("modal");
+    if (display) {
+      modal!.classList.remove("hide");
+    } else {
+      modal!.classList.add("hide");
+    }
+  };
+
+  const editTask = (task: ITask): void => {
+    hideOrShowModal(true);
+    setTaskToUpdate(task);
+  };
+
   return (
     <div>
+      <Modal
+        children={
+          <TaskForm
+            btnText="Editar Tarefa"
+            taskList={taskList}
+            task={taskToUpdate}
+          />
+        }
+      />
       <Header />
       <main className={styles.main}>
         <div>
@@ -33,7 +58,11 @@ function App() {
         </div>
         <div>
           <h2>Suas tarefas</h2>
-          <TaskList taskList={taskList} handleDelete={deleteTask} />
+          <TaskList
+            taskList={taskList}
+            handleDelete={deleteTask}
+            handleEdit={editTask}
+          />
         </div>
       </main>
       <Footer />
